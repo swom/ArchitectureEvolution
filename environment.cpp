6b3fbcfcb971a58c158d8d6079bd3ce508f98d25
Clem Carle <clem.carle@gmail.com>
2021-10-21 14:30:14 +0200

    added tentative test for #11

--- environment.cpp
+++ environment.cpp
@@ -230,5 +230,24 @@ void test_environment() noexcept
         assert(env_series == tester_series);
     }
 #endif
+
+
+//#define FIX_ISSUE_11
+#ifdef FIX_ISSUE_11
+    {
+        environment e{env_param{}};
+        std::mt19937_64 rng;
+        std::vector<double> inputs = create_n_inputs(e, 3, rng);
+
+        std::function<double(std::vector<double>)> env_function;
+
+        double optimal_output = env_function(inputs);
+
+        assert(optimal_output == calc_optimal_output(inputs, env_function));
+    }
+#endif
+
+
+
 }
 #endif
