c4a7333cd0a074fc7da5262ae48d7a17e149ce99
swom <P288427@Workspace.rug.nl>
2021-12-08 15:01:12 +0100

    creating wrapper function to run different kind of simulations based on mutation_type

--- parser.cpp
+++ parser.cpp
@@ -3,8 +3,42 @@
 #include <cassert>
 #include <map>
 
+void run_simulation_given_arguments(const cxxopts::ParseResult& results)
+{
+    auto mut_type = convert_ind_args(results).m_mutation_type;
 
+    if(mut_type == mutation_type::weights)
+    {
 
+        observer<mutation_type::weights> o;
+        auto s = create_simulation<mutation_type::weights>(results);
+        exec<mutation_type::weights>(s, o) ;
+        save_json(o,
+                  convert_arc_to_string(o.get_params().i_p.net_par.net_arc) +
+                  "_" + std::to_string(o.get_params().s_p.seed) + ".json");
+    }
+    else if (mut_type == mutation_type::activation) {
+
+        observer<mutation_type::activation> o;
+        auto s = create_simulation<mutation_type::activation>(results);
+        exec<mutation_type::activation>(s, o) ;
+        save_json(o,
+                  convert_arc_to_string(o.get_params().i_p.net_par.net_arc) +
+                  "_" + std::to_string(o.get_params().s_p.seed) + ".json");
+    }
+    else if (mut_type == mutation_type::weights_and_activation) {
+        observer<mutation_type::weights_and_activation> o;
+        auto s = create_simulation<mutation_type::weights_and_activation>(results);
+        exec<mutation_type::weights_and_activation>(s, o) ;
+        save_json(o,
+                  convert_arc_to_string(o.get_params().i_p.net_par.net_arc) +
+                  "_" + std::to_string(o.get_params().s_p.seed) + ".json");
+    }
+    else
+    {
+        throw std::runtime_error{"unknown mutation type"};
+    }
+}
 
 ///NOT tested!!!
 env_param convert_env_args(const cxxopts::ParseResult& results)
