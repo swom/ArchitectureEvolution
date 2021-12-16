bda706a994037910b042c3645dbd8420527f00a3
Clem Carle <clem.carle@gmail.com>
2021-12-16 11:38:12 +0100

    solved #213

--- mutation_type.cpp
+++ mutation_type.cpp
@@ -1,5 +1,36 @@
 #include "mutation_type.h"
 #include "cassert"
+
+std::string convert_mut_type_to_string(mutation_type m)
+{
+    std::string string;
+
+    switch (m) {
+    case mutation_type::weights :
+        string = "weights";
+        return string;
+        break;
+
+    case mutation_type::weights_and_activation :
+        string = "weights_and_activation";
+        return string;
+        break;
+
+    case mutation_type::activation :
+        string = "activation";
+        return string;
+        break;
+
+    case mutation_type::duplication :
+        string = "duplication";
+        return string;
+        break;
+
+    default:
+        throw 2;
+    }
+}
+
 void test_mutation_type()
 {
     assert(mutation_type::activation != mutation_type::weights &&
