f585482077eb809446133d80779de616ac5b5d59
swom <P288427@Workspace.rug.nl>
2022-01-26 11:47:28 +0100

    fixing silly mistakes

--- range.cpp
+++ range.cpp
@@ -6,7 +6,7 @@ range::range(double start, double end):
     m_start{start},
     m_end{end}
 {
- if(m_start < m_end)
+ if(m_start > m_end)
  {
      throw std::runtime_error{"invalid cue range"};
  }
--- simulation.cpp
+++ simulation.cpp
@@ -411,7 +411,7 @@ void test_simulation() noexcept//!OCLINT test may be many
     {
         //create a non-default simulaiton
         env_param e_p{};
-        e_p.cue_distrib = std::vector<double>{-0.123,0.123};
+        e_p.cue_distrib = range{-0.123,0.123};
         net_param n_p;
         n_p.net_arc = {1,2,3,4,5,6};
         n_p.max_arc = {1,2,3,4,5,6};
