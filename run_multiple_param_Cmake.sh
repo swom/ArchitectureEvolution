f223a09f24d1915461f01d2103173d74ef7011eb
swom <P288427@Workspace.rug.nl>
2022-02-07 13:19:42 +0100

    see if I cna build using Cmake

--- run_multiple_param_Cmake.sh
+++ run_multiple_param_Cmake.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+# Script to run multiple simulations
+#
+# Usage, locally:
+#
+#   ./run_multiple_param_loop.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./run_multiple_param_loop.sh
+#
+# Peregrine directives:
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=run_arc_evo_loop
+#SBATCH --output=run_arc_evo_loop.log
+
+module load CMake
 CC=clang 
 CXX=clang++ 
 cmake -S . -B build
