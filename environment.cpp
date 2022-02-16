db7cde5c4a44525032b441891b2a6923cd05721c
Clem Carle <clem.carle@gmail.com>
2022-02-16 19:30:48 +0100

    changed default function

--- environment.cpp
+++ environment.cpp
@@ -24,8 +24,8 @@ environment::environment(const env_param& e_p):
     m_cue_distribution{m_cue_range.m_start, m_cue_range.m_end},
     m_env_function_A{e_p.env_function_A},
     m_env_function_B{e_p.env_function_B},
-    m_current_function{e_p.env_function_A},
-    m_name_current_function{'A'}
+    m_current_function{e_p.env_function_B},
+    m_name_current_function{'B'}
 {
     m_rng.seed(0);
 }
