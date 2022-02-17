d3591f9423f11e1e046b346160da2acff9d78ed4
swom <P288427@Workspace.rug.nl>
2022-02-17 12:00:01 +0100

    changing the main .sh file for Cmake so tha it works

--- run_multiple_param_Cmake.sh
+++ run_multiple_param_Cmake.sh
@@ -18,6 +18,8 @@
 #SBATCH --job-name=run_arc_evo_loop
 #SBATCH --output=run_arc_evo_loop.log
 
+# to get arc_evo.pro command interface 
+#run arc_evo.exe with command line argument -h 
 module load CMake
 module load binutils
 mkdir build
@@ -25,14 +27,18 @@ cd build
 cmake ..
 cmake --build . 
 
-declare -a architectures=("1,2,2,2,1")
+declare -a architectures=("1,2,1" "1,2,2,2,1" "1,8,8,8,1")
 declare -a max_architectures=("1,8,8,8,1")
 declare -a gen=(1000000)
-declare -a change_freq_As=(0 0.005 0.001 0.01)
-declare -a mut_types=("NRduplication" "NRaddition" "weights_and_activation" "weights")
-declare -a change_types=("regular")
+declare -a change_types=("asymmetrical")
+declare -a change_freq_As=(0)
+#declare -a change_freq_Bs=(0.0005)
+declare -a selection_types=("sporadic")
+declare -a selection_freqs=(100 1000 10000) 
+declare -a mut_types=("weights")
 declare -a dup_rates=(0.0001)
 declare -a act_rates=(0.001)
+
 
 for seed in $(seq 1 10)
 do
@@ -40,35 +46,32 @@ do
 	do
 		for max_arc in "${max_architectures[@]}"
 		do
-			for change_freq_A in "${change_freq_A[@]}"
+			for change_freq_A in "${change_freq_As[@]}"
 			do
 				for gen in "${gen[@]}"
 				do	
 					for mut_type in "${mut_types[@]}"
 					do
-						for change_type in "${change_types[@]}"
-						do
-							for dup_rate in "${dup_rates[@]}"
-							do
-								for act_rate in "${act_rates[@]}"
-								do
-									   echo "seed:  "$seed
-									   echo "architecture:  "$arc
-									   echo "max_architecture:  "$max_arc
-									   echo "change_freq_A:  "$change_freq_A
-									   echo "number of generations:  "$gen
-									   echo "mutation type:  "$mut_type
-									   echo "environmental change type:  "$change_type
-									   echo "duplication rate:  "$dup_rate
-									   echo "activation rate:  "$act_rate
-						  
-									  sbatch ../run_loop.sh $seed $arc $max_arc $change_freq_A $gen $mut_type $change_type $dup_rate $act_rate 
-								done
-							done
-						done
+            for change_type in "${change_types[@]}"
+            do
+              for dup_rate in "${dup_rates[@]}"
+              do
+                for act_rate in "${act_rates[@]}"
+                do
+                  for selection_type in "${selection_types[@]}"
+		              do 
+                    for selection_freq in "${selection_freqs[@]}"
+  								  do    						  
+    									  echo $seed $arc $max_arc $change_freq_A $gen $mut_type $change_type $dup_rate $act_rate $selection_type $selection_freq
+                        sbatch ../run_loop.sh $seed $arc $max_arc $change_freq_A $gen $mut_type $change_type $dup_rate $act_rate $selection_type $selection_freq
+    								done 
+                  done 
 					done
 				done
 			done
-		done 
+		done
 	done
+done 
+done
+done
 done
