c91939e02918dc866fedd04baad7325f58c98d80
swom <P288427@Workspace.rug.nl>
2022-03-03 16:57:39 +0100

    adding extra template argument for regular or stochastic change

--- src/launcher_functions.h
+++ src/launcher_functions.h
@@ -29,7 +29,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -42,7 +42,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -55,7 +55,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -68,7 +68,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -81,7 +81,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -94,7 +94,7 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
         using sim_t = simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>;
 
         observer<sim_t> o;
-        auto s = create_simulation<pop_t, env_change_sym_type, selection_type>(results);
+        auto s = create_simulation<pop_t, env_change_sym_type, env_change_freq_type, selection_type>(results);
         exec<sim_t>(s, o) ;
         save_json(o,
                   create_save_name_from_observer_data(o));
@@ -108,12 +108,12 @@ void run_simulation_given_mut_type(const cxxopts::ParseResult& results)
 template<env_change_freq_type E, selection_type S>
 void run_simulation_given_env_change_type(const cxxopts::ParseResult& results)
 {
-    auto env_change_type = convert_sim_args(results).change_type ;
-    if(env_change_type == env_change_symmetry_type::asymmetrical)
+    auto env_change_symmetry_type = convert_sim_args(results).change_sym_type ;
+    if(env_change_symmetry_type == env_change_symmetry_type::asymmetrical)
     {
         run_simulation_given_mut_type<env_change_symmetry_type::asymmetrical, E, S>(results);
     }
-    else if(env_change_type == env_change_symmetry_type::symmetrical)
+    else if(env_change_symmetry_type == env_change_symmetry_type::symmetrical)
     {
         run_simulation_given_mut_type<env_change_symmetry_type::symmetrical, E, S>(results);
     }
@@ -141,14 +141,14 @@ void run_simulation_given_sel_type(const cxxopts::ParseResult& results)
 
 void run_simulation_given_env_freq_type(const cxxopts::ParseResult& results)
 {
-    auto sel_type = convert_sim_args(results).sel_type ;
-    if(sel_type == selection_type::constant)
+    auto env_change_freq_type = convert_sim_args(results).change_freq_type ;
+    if(env_change_freq_type == env_change_freq_type::regular)
     {
-        run_simulation_given_env_change_type<E,selection_type::constant>(results);
+        run_simulation_given_sel_type<env_change_freq_type::regular>(results);
     }
-    else if(sel_type == selection_type::sporadic)
+    else if(env_change_freq_type == env_change_freq_type::stochastic)
     {
-        run_simulation_given_env_change_type<E, selection_type::sporadic>(results);
+        run_simulation_given_sel_type<env_change_freq_type::stochastic>(results);
     }
     else{
         throw std::runtime_error{"unknown change type"};
