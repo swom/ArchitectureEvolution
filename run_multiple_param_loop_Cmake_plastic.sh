12edebf2b7136213d5dcb9fcc37696aebac00de0
swom <P288427@Workspace.rug.nl>
2022-03-23 15:03:53 +0100

    adding .sh script for plasticity with asymmetric change and adaptation period

---
+++ run_multiple_param_loop_Cmake_plastic.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Script to run multiple simulations
+#
+# Usage, locally:
+#
+#   ./run_multiple_param_loop.sh
+#
+# Usage, on Peregrine:
+#
+#   sbatch ./run_multiple_param_loop.sh
+#
+# Peregrine directives:
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1GB
+#SBATCH --job-name=run_arc_evo_loop
+#SBATCH --output=run_arc_evo_loop.log
+
+
+# to get arc_evo.pro command interface 
+#run arc_evo.exe with command line argument -h 
+module load git
+module load CMake
+module load binutils
+mkdir build
+cd build
+cmake ..
+cmake --build . 
+
+
+declare -a architectures=("2,2,2,2,1")
+declare -a max_architectures=("2,2,2,2,1")
+declare -a change_freq_As=(0.1)
+declare -a change_freq_Bs=(0.01)
+declare -a gen=(1000000)
+declare -a mut_types=("weights")
+declare -a sel_types=("constant")
+declare -a change_freq_types=("regular")
+declare -a adaptation_periods=("on" "off")
+declare -a sel_freqs=(0 100 1000 10000)
+declare record_top_ind_freq=1000
+declare n_observations_reaction_norm=100
+declare n_trials=10
+
+for seed in $(seq 1 10)
+do
+	for arc in "${architectures[@]}"
+	do
+		for max_arc in "${max_architectures[@]}"
+		do
+			for change_freq_A in "${change_freq_As[@]}"
+			do
+				for gen in "${gen[@]}"
+				do	
+					for mut_type in "${mut_types[@]}"
+					do
+						for sel_type in "${sel_types[@]}"
+						do
+							for sel_freq in "${sel_freqs[@]}"
+							do			
+								for adaptation_period in "${adaptation_periods[@]}"
+								do
+									for change_freq_type in "${change_freq_types[@]}"
+									do
+											echo $seed $arc $max_arc $change_freq_A $gen $mut_type $sel_type $sel_freq $adaptation_period $change_freq_types
+											sbatch ../run_loop_sporadic.sh $seed $arc $max_arc $change_freq_A $gen $mut_type $sel_type $sel_freq $record_top_ind_freq $n_observations_reaction_norm $n_trials $adaptation_period $change_freq_type
+									done
+								done
+							done
+						done
+					done
+				done 
+			done
+		done
+	done
+done
